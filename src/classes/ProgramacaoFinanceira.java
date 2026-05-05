fc5c706f73cd42e5291cc251fc233f94ce469dbc
saulo <ti@hdnbombas.com.br>
2026-05-05 09:52:25 -0300

    first commit

---
+++ src/classes/ProgramacaoFinanceira.java
@@ -0,0 +1,51 @@
+package classes;
+
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+
+public class ProgramacaoFinanceira implements EventoProgramavelJava {
+
+	@Override
+	public void afterDelete(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterInsert(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterUpdate(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeCommit(TransactionContext event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeDelete(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeInsert(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeUpdate(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
